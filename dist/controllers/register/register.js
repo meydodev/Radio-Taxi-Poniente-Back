35b0f3b2cc2a4b1b7b1f88f5dc5a272c8bd23404
meydodev <meydodev@gmail.com>
2024-12-17 17:02:40 +0100

    second commit

---
+++ dist/controllers/register/register.js
@@ -0,0 +1,77 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const bcrypt_1 = __importDefault(require("bcrypt"));
+const db_1 = __importDefault(require("../../db/db"));
+const router = express_1.default.Router();
+const saltRounds = 12;
+router.post('/new-user', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        // Validar los datos de entrada
+        const { name, surnames, password, email, license, keyAccess } = req.body;
+        if (!email || !password || !name || !surnames || !license) {
+            res.status(400).json({ message: 'Todos los campos son obligatorios' });
+            return;
+        }
+        if (keyAccess !== 'TaxiRadio') {
+            res.status(400).json({ message: 'Clave de acceso incorrecta' });
+            return;
+        }
+        const hashedPassword = yield bcrypt_1.default.hash(password, saltRounds);
+        const usuario = { name, surnames, email, license, password: hashedPassword };
+        // Iniciar la transacción
+        db_1.default.beginTransaction((err) => {
+            if (err) {
+                console.error('Error al iniciar la transacción:', err);
+                res.status(500).json({ message: 'Error al iniciar la transacción' });
+                return;
+            }
+            // Insertar el usuario en la base de datos
+            const query = `INSERT INTO users SET ?`;
+            db_1.default.query(query, usuario, (error, results) => {
+                if (error) {
+                    console.error('Error al insertar el usuario:', error);
+                    if (error.code === 'ER_DUP_ENTRY') {
+                        db_1.default.rollback(() => {
+                            res.status(400).json({ message: 'El correo electrónico ya está registrado' });
+                        });
+                    }
+                    else {
+                        db_1.default.rollback(() => {
+                            res.status(500).json({ message: 'Error al insertar el usuario' });
+                        });
+                    }
+                    return;
+                }
+                // Confirmar la transacción
+                db_1.default.commit((err) => {
+                    if (err) {
+                        console.error('Error al confirmar la transacción:', err);
+                        db_1.default.rollback(() => {
+                            res.status(500).json({ message: 'Error al confirmar la transacción' });
+                        });
+                        return;
+                    }
+                    res.status(200).json({ message: 'Usuario registrado correctamente' });
+                });
+            });
+        });
+    }
+    catch (error) {
+        console.error('Error al registrar el usuario:', error);
+        res.status(500).json({ message: 'Error interno del servidor' });
+    }
+}));
+exports.default = router;
--- dist/index.js
+++ dist/index.js
@@ -4,8 +4,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const register_1 = __importDefault(require("./controllers/register/register"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
+app.use('/register', register_1.default);
 app.listen(3000, () => {
     console.log('Servidor corriendo por el puerto 3000');
 });
