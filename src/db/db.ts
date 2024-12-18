3ff6d289bd0eb76699246885e975c567b2905207
meydodev <meydodev@gmail.com>
2024-12-18 12:50:09 +0100

    first commit

--- src/db/db.ts
+++ src/db/db.ts
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'app_taxi_poniente',
+    database: 'radio_taxi_poniente',
 });
 
 export default connection;
