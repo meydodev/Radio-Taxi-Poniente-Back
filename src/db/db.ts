0e8bfc65d0c96e1c0d2e498f2f797e3a3e3f50e6
meydodev <meydodev@gmail.com>
2025-01-24 21:33:26 +0100

    second commit

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,19 +1,21 @@
 import mysql from 'mysql2'
 
-/*
+
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
     database: 'radio_taxi_poniente',
 });
-*/
+
+/*
 const connection = mysql.createConnection({
     host: 'bhkq4ktdt8hu8why4ffk-mysql.services.clever-cloud.com',
     user: 'uc7nsrkdmz5abri9',
     password: 'j1kJ3RAOsRfCCC5pqCem',
     database: 'bhkq4ktdt8hu8why4ffk',
 });
+*/
 
 
 export default connection;
